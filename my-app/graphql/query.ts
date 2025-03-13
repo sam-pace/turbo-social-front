09779abb6a8deb19f9ab1931fd3aeccb3b7749ac
sam-pace <sl.spacce2000@gmail.com>
2025-03-13 19:22:29 -0300

    feat: added new queries

--- my-app/graphql/query.ts
+++ my-app/graphql/query.ts
@@ -1,11 +1,51 @@
 import { gql } from "@apollo/client";
 
 export const GET_POSTS = gql`
-  query {
-    postAll {
+  query PostAll($userId: String!) {
+    postAll(userId: $userId) {
+      id
+      content
+      createdAt
+      user {
+        avatarUrl
+        username
+      }
+      comments {
+        content
+      }
+      imageUrl
+      likes
+      isLiked
+      updatedAt
+      userId
+    }
+  }
+`;
+
+export const GET_POST_BY_ID = gql`
+  query Post($id: String!) {
+    post(id: $id) {
+      user {
+        avatarUrl
+        username
+      }
       id
       content
       likes
+    }
+  }
+`;
+
+export const GET_COMMENTS_BY_POST = gql`
+  query CommentsByPost($postId: String!) {
+    commentsByPost(postId: $postId) {
+      avatarUrl
+      content
+      createdAt
+      id
+      postId
+      userId
+      username
     }
   }
 `;
@@ -15,6 +55,10 @@ export const GET_TURBOMARKET = gql`
     turboMarketAll {
       id
       userId
+      user {
+        username
+        avatarUrl
+      }
       title
       description
       price
@@ -26,6 +70,39 @@ export const GET_TURBOMARKET = gql`
       transmission
       imageUrl
       location
+    }
+  }
+`;
+
+export const GET_TURBOMARKET_ITEM = gql`
+  query TurboMarket($id: String!) {
+    turboMarket(id: $id) {
+      id
+      title
+      description
+      price
+      imageUrl
+      brand
+      model
+      year
+      mileage
+      fuelType
+      transmission
+      location
+      user {
+        username
+        avatarUrl
+      }
+    }
+  }
+`;
+
+export const GET_USER = gql`
+  query User($id: String!) {
+    user(id: $id) {
+      id
+      username
+      avatarUrl
     }
   }
 `;
