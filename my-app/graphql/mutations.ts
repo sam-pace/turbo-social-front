4c03de61ff0ea56c8ed23fe41845cae6dab4b992
sam-pace <sl.spacce2000@gmail.com>
2025-03-13 19:22:40 -0300

    feat: added new mutations

--- my-app/graphql/mutations.ts
+++ my-app/graphql/mutations.ts
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client/core";
 
 export const LIKE_POST = gql`
-  mutation LikePost($input: UpdatePostInput!) {
-    updatePost(updatePost: $input) {
+  mutation HandleLikes($updatePost: UpdatePostInput!) {
+    handleLikes(updatePost: $updatePost) {
       id
       likes
     }
@@ -15,6 +15,36 @@ export const LOGIN_MUTATION = gql`
       accessToken
       refreshToken
       message
+    }
+  }
+`;
+
+export const REFRESH_TOKEN = gql`
+  mutation RefreshToken($refreshToken: String!) {
+    refreshToken(refreshToken: $refreshToken)
+  }
+`;
+
+export const CREATE_TURBOMARKET = gql`
+  mutation CreateTurboMarket($data: CreateTurboMarketInput!) {
+    createTurboMarket(data: $data) {
+      id
+    }
+  }
+`;
+
+export const CREATE_USER = gql`
+  mutation CreateUser($data: CreateUserInput!) {
+    createUser(data: $data) {
+      id
+    }
+  }
+`;
+
+export const CREATE_COMMENT = gql`
+  mutation CreateComment($input: CreateCommentInput!) {
+    createComment(createComment: $input) {
+      id
     }
   }
 `;
